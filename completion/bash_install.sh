fa545b043e9b15b9d842e8b1fa5af72ad68ee15d
DragonStuff <me@0xdragon.com>
2020-03-17 17:08:41 +0900

    fix: add lint to bash script

--- completion/bash_install.sh
+++ completion/bash_install.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 git clone https://github.com/tablecheck/kubectl-application-shell.git ~/.kubeas
 
 COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
 ln -sf ~/.kubeas/completion/kubectl-application-shell.bash $COMPDIR/kubeas
 cat << FOE >> ~/.bashrc
-
 
 #kubeas
 export PATH=~/.kubeas:\$PATH
