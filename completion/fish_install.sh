589b0cb5ab82230cc2186fd14462098268d186db
DragonStuff <me@0xdragon.com>
2020-03-17 14:38:14 +0900

    autocomplete: fix fish install

--- completion/fish_install.sh
+++ completion/fish_install.sh
@@ -1,7 +1,7 @@
-# shellcheck disable=SC1008
 #!/usr/local/bin/fish
+# shellcheck disable=SC1008
 
 git clone https://github.com/tablecheck/kubectl-application-shell.git ~/.kubeas
 
 mkdir -p ~/.config/fish/completions
-ln -s ~/kubeas/completion/kubeas.fish ~/.config/fish/completions/
+ln -s ~/.kubeas/completion/kubectl-application-shell.fish ~/.config/fish/completions/kubeas.fish
