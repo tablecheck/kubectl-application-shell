54353590a9fca54cc18acaef66d41ee51acb2405
DragonStuff <me@0xdragon.com>
2020-03-17 17:11:01 +0900

    fix: prevent word splitting

--- completion/bash_install.sh
+++ completion/bash_install.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/tablecheck/kubectl-application-shell.git ~/.kubeas
 
 COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
-ln -sf ~/.kubeas/completion/kubectl-application-shell.bash $COMPDIR/kubeas
+ln -sf ~/.kubeas/completion/kubectl-application-shell.bash "$COMPDIR"/kubeas
 cat << FOE >> ~/.bashrc
 
 #kubeas
