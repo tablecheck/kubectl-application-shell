0df47af8083e2d0a2a00234d29fce35bef97f6f8
DragonStuff <me@0xdragon.com>
2020-03-17 13:30:39 +0900

    feature: add bash autocomplete shell

--- completion/bash_install.sh
+++ completion/bash_install.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/tablecheck/kubectl-application-shell.git ~/.kubeas
 
 COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
-ln -sf ~/.kubeas/completion/kubeas.bash $COMPDIR/kubeas
+ln -sf ~/.kubeas/completion/kubectl-application-shell.bash $COMPDIR/kubeas
 cat << FOE >> ~/.bashrc
 
 
