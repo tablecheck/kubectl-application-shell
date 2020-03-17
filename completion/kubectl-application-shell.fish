bdb5120caaf071509ef621ce574e90de1aa8b233
DragonStuff <me@0xdragon.com>
2020-03-17 17:03:02 +0900

    fix: do not display autocomplete for namespaces if name is specified

--- completion/kubectl-application-shell.fish
+++ completion/kubectl-application-shell.fish
@@ -1,2 +1,2 @@
-complete -f -c kubeas -n '__fish_contains_opt name' -a "(kubectl get deployments -o=name)" 
-complete -f -c kubeas -n '__fish_contains_opt namespace' -a "(kubectl get namespaces -o=name)"
+complete -f -c kubeas -n '__fish_contains_opt name' -a "(kubectl get deployments -ocustom-columns=":.metadata.name")" 
+complete -f -c kubeas -n '__fish_contains_opt namespace; and not __fish_contains_opt name' -a "(kubectl get namespaces -ocustom-columns=":.metadata.name")"
